e0a87bbb57c033b706014c61a53ac6358ba68203
EmilieFlores <codingtime0000@gmail.com>
2022-12-23 14:08:15 -0600

    update

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const passport = require('passport')
 // const session = require('express-session')
 require('dotenv').config({path: './config/.env'})
 const connectDB = require('./config/database')
-// const homeRoute = require('./routes/home-router')
+const homepageRoute = require('./routes/homepage-route')
 // const editRoute = require('./routes/edit-router')
 // const signupRoute = require('./routes/signup-router')
 const PORT = 2121
 
 connectDB()
+
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}));
+
+
+app.use("/", homepageRoute)
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`)
