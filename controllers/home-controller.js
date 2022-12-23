e44d47ea2b4ab1d98dfc43154b5a28dca8216f18
Aaron <soteloaaron16@gmail.com>
2022-12-23 14:10:22 -0600

    home controller

---
+++ controllers/home-controller.js
@@ -0,0 +1,31 @@
+const quotes = require('../models/quotes')
+
+module.exports = {
+    getIndex : async (req, res) => {
+        try{
+            const oneLiners = await
+            quotes.find()
+            res.render('index.ejs', { quotes: oneLiners })
+        } catch (error) {
+            res.status(500).send({message: error.message})
+        }
+    },
+
+    createOneLiner : async (req, res) => {
+        const addOneLiner = new quotes(
+            {
+                quote: req.body.quote,
+                name: req.body.name
+            }
+        )
+        try {
+            await addOneLiner.save()
+            console.log(addOneLiner)
+            res.redirect('/')
+        } catch (err) {
+            if (err) return res.status(500).send(err)
+            res.redirect('/')
+        }
+        }
+
+}
