68f1221aa60a15ce83577dd67e08e2e1bdca3eeb
Aaron <soteloaaron16@gmail.com>
2022-12-23 12:52:27 -0600

    server additions

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+// const session = require('express-session')
+require('dotenv').config({path: './config/.env'})
+const connectDB = require('./config/database')
+const homeRoute = require('./routes/home-router')
+const editRoute = require('./routes/edit-router')
+const signupRoute = require('./routes/signup-router')
+const PORT = 2121
