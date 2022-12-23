73d6852760180dbe5b8ad37872e2c468b82d1a92
EmilieFlores <codingtime0000@gmail.com>
2022-12-23 14:27:45 -0600

    update

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -3,29 +3,29 @@ const food = require('../models/food')
 module.exports = {
     getIndex : async (req, res) => {
         try{
-            const food = await
-            quotes.find()
-            res.render('index.ejs', { place: food })
+            const fooditem = await
+            food.find()
+            res.render('index.ejs', { food: fooditem })
         } catch (error) {
             res.status(500).send({message: error.message})
         }
     },
 
-    createOneLiner : async (req, res) => {
-        const addOneLiner = new quotes(
-            {
-                quote: req.body.quote,
-                name: req.body.name
-            }
-        )
-        try {
-            await addOneLiner.save()
-            console.log(addOneLiner)
-            res.redirect('/')
-        } catch (err) {
-            if (err) return res.status(500).send(err)
-            res.redirect('/')
-        }
-        }
+    // createOneLiner : async (req, res) => {
+    //     const addOneLiner = new quotes(
+    //         {
+    //             quote: req.body.quote,
+    //             name: req.body.name
+    //         }
+    //     )
+    //     try {
+    //         await addOneLiner.save()
+    //         console.log(addOneLiner)
+    //         res.redirect('/')
+    //     } catch (err) {
+    //         if (err) return res.status(500).send(err)
+    //         res.redirect('/')
+    //     }
+    //     }
 
 }
